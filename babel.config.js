9699abc44d841807b8e61db40f029c50d2f22c9e
Tim <jasper7466@yandex.ru>
2019-12-01 15:36:00 +0500

    Babel. Добавлена поддержка требуемых браузеров. В требованиях явно не указано до какой версии следует обеспечить поддержку, поэтому для каждого браузера задал предыдущую относительно текущей актуальной версию. Информация об актуальных версиях взята отсюда: https://caniuse.com/#comparison

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,14 @@ const presets = [
 	[
 		"@babel/env",
 		{
+			targets:
+			{
+				chrome: "80",
+				chromeandroid: "78",
+				firefox: "71",
+				safari: "13",
+				edge: "18"
+            },
 			useBuiltIns: "usage",
 			corejs: "3.4.1",
 			"targets":
@@ -9,7 +17,7 @@ const presets = [
 				"esmodules": true,
 				"ie": "11"
 			}
-		},
+		}
 	],
 ];
 
