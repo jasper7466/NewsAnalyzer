a328d059282ea98fddeb16e00e401cb618dce5d3
Tim <jasper7466@yandex.ru>
2019-12-18 12:38:39 +0500

    Вёрстка. Страница "About". Установлен Swiper

--- src/scripts/about.js
+++ src/scripts/about.js
@@ -1,1 +1,21 @@
-import "../styles/about.css";
+import '../styles/about.css';
+// import '../../node_modules/swiper/js/swiper.min.js';
+import Swiper from 'swiper';
+
+var mySwiper = new Swiper('.history__container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
