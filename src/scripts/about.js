fac78e04d1aff6bfbe8ddbad3cfa70297e68d0fa
Tim <jasper7466@yandex.ru>
2019-12-20 01:05:57 +0500

    Вёрстка. Слайдер. Остановился на Flickity, сделал затенение по краям - удалены лишние слайдеры - немного подчищен код после экспериментов - реализовано затенение крайних слайдов

--- src/scripts/about.js
+++ src/scripts/about.js
@@ -1,52 +1,6 @@
 import '../styles/about.css';
 
-import Swiper from 'swiper';
 import Flickity from 'flickity';
-import Glide from '@glidejs/glide';
 
 let flkty = new Flickity( '.main-carousel', {
-  // options
 });
-
-const glider = new Glide('.glide', {
-  // type: 'carousel',
-  startAt: 0,
-  perView: 3,
-})
-
-
-
-glider.enable()
-
-glider.mount();
-
-let commitSwiper = new Swiper('.history__container', {
-  direction: 'horizontal',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    // clickable: true,
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  speed: 100,
-  spaceBetween: 16,
-  // setWrapperSize: true,
-  // width: 1000,
-
-  slidesPerView: 4,
-  // slidesPerGroup: 3,
-  centeredSlides: true,
-  // centeredSlidesBounds: true,
-  // updateOnWindowResize: true,
-  centerInsufficientSlides: true,
-});
-
-commitSwiper.width = 50;
